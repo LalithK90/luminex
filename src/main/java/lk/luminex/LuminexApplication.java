03281f6e4c562a86bfcacbdabcebca2aa71bdffb
LalithK90 <asakahatapitiya@gmail.com>
2020-05-10 13:17:38 +0530

    Initial commit

---
+++ src/main/java/lk/luminex/LuminexApplication.java
@@ -0,0 +1,13 @@
+package lk.luminex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/luminex/luminex/LuminexApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.luminex.luminex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
